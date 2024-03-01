5ca4f46b02c73a8d2515f97f4c3c39d4f61fc46e
Nick Ahn <nickahn01@gmail.com>
2024-03-01 03:21:41 -0800

    Create connection to postgresql database and rename go modules

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/pacific-theta-tau/tt-db/db"
 )
 
 func main() {
+	dbConn, err := db.ConnectPostgresDB()
+	if err != nil {
+		fmt.Println("Error connecting to database:", err)
+	}
+	defer dbConn.Close(context.Background())
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World!"))
 	})
+
 	http.ListenAndServe(":3000", r)
 }
--- go.mod
+++ go.mod
@@ -1,13 +1,16 @@
-module github.com/Pacific-Theta-Tau/TT-DB
+module github.com/pacific-theta-tau/tt-db
 
 go 1.22.0
 
-require github.com/go-chi/chi/v5 v5.0.12
+require (
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/jackc/pgx/v5 v5.5.3
+	github.com/joho/godotenv v1.5.1
+)
 
 require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.5.3 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
