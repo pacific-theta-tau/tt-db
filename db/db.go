fb743e370defe7bffdcbc58e0b17393f83027be2
Nick Ahn <nickahn01@gmail.com>
2024-03-01 03:20:42 -0800

    Add connectPostgresDB()

--- db/db.go
+++ db/db.go
@@ -2,24 +2,27 @@ package db
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
-type DB struct {
-	DB *sql.DB
-}
+func ConnectPostgresDB() (*pgx.Conn, error) {
+	// Load DB configs from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error in loading .env file")
+	}
 
-var dbConn = &DB{}
-
-func ConnectPostgresDB(dsn string) (*DB, error) {
+	// Connect to postgres DB
 	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	defer conn.Close(context.Background())
+
+	return conn, nil
 }
