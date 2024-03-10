1390d929c4d660ff7a9b067aaa32d344c36f8a59
Nick Ahn <nickahn01@gmail.com>
2024-03-10 01:22:08 -0800

    Fix database connection

---
+++ api/server.go
@@ -0,0 +1,48 @@
+// This package defines server and routes
+package api
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/pacific-theta-tau/tt-db/db"
+)
+
+type Config struct {
+	Port        string
+	DatabaseURL string
+}
+
+type Application struct {
+	Database *db.PostgresDB
+	Config   Config
+}
+
+func NewApplication(config Config, db *db.PostgresDB) *Application {
+	return &Application{
+		Config:   config,
+		Database: db,
+	}
+}
+
+// Start server and routers for the application
+func (app *Application) Serve() {
+	// Connect to database
+	app.Database.Connect(app.Config.DatabaseURL)
+
+	// Start routers and middleware
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+
+	//TODO: cleaner address
+	addr := "localhost:" + app.Config.Port
+	err := http.ListenAndServe(addr, r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
