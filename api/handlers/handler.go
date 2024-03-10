48a0bfde8a0f2f82278990a97e9b419336811d3c
Nick Ahn <nickahn01@gmail.com>
2024-03-10 03:39:59 -0700

    Add get request for all brothers info

--- api/handlers/handler.go
+++ api/handlers/handler.go
@@ -1,12 +1,17 @@
 package handlers
 
-import "database/sql"
+import (
+	"database/sql"
+	"time"
+)
 
-// Used to share database connection across all handler functions
+const dbTimeout = time.Second * 3
+
 type Handler struct {
 	db *sql.DB
 }
 
+// Create a new Handler instance with the app's database connection
 func NewHandler(db *sql.DB) *Handler {
 	return &Handler{db: db}
 }
