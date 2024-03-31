b3444326123613c940a036b10e04ca90ed977e08
thlau21 <thomaslau315@gmail.com>
2024-03-31 15:35:55 -0700

    Added sql to create Events table for docker

--- db/scripts/init.sql
+++ db/scripts/init.sql
@@ -15,3 +15,15 @@ CREATE TABLE IF NOT EXISTS brothers(
 
 INSERT INTO brothers (pacificId, firstName, lastName, status, className, rollCall, email, phoneNumber, badStanding)
 VALUES (989123456, 'Nicolas', 'Ahn', 'Active', 'Chi', '000', 'na@gmail.com', '(123) 456-7890', 0);
+
+
+CREATE TABLE Events(
+  event_id INT PRIMARY KEY NOT NULL, 
+  event_name TEXT NOT NULL,
+  category INT NOT NULL,
+  locations Text NOT NULL,
+  dates date NOT NULL
+);
+
+INSERT INTO Events (event_id, event_name, category, locations, dates)
+VALUES (1, 'Meet the Bros', 5, 'Ballroom', '1/28/24');
