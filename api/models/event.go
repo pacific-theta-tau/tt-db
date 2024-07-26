d01fa370008c0a63cc2f8315bb4967696f85426c
Nick Ahn <nickahn01@gmail.com>
2024-07-26 13:02:31 +0900

    Update event model to include categoryName

--- api/models/event.go
+++ api/models/event.go
@@ -5,8 +5,8 @@ import("time")
 // TODO: decide which fields are nullable.
 type Event struct {
 	EventID			int			`json:"eventID"`//primary
-	Category		int			`json:"category"` 
 	EventName		string 		`json:"eventName"`
+	CategoryName	string      `json:"categoryName"` 
 	EventLocation	string 		`json:"eventLocation"`
 	EventDate		time.Time	`json:"eventDate"`
 }
