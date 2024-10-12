eaabcc0239f9f15461991db3f4de2360ff2a58ec
Nick Ahn <nickahn01@gmail.com>
2024-10-13 00:08:46 +0900

    Add brother status per semester struct

--- api/models/semester.go
+++ api/models/semester.go
@@ -7,7 +7,7 @@ type Semester struct {
     SemesterLabel   string `json:"semesterLabel"`
 }
 
-// Helper function to scan SQL row and create new Brother instance
+// Helper function to scan SQL row and create new Semester instance
 func CreateSemesterFromRow(row *sql.Rows) (Semester, error) {
     var semester Semester
 	err := row.Scan(
@@ -19,4 +19,31 @@ func CreateSemesterFromRow(row *sql.Rows) (Semester, error) {
 	}
 
 	return semester, err
+}
+
+type BrotherStatusFromSemester struct {
+    RollCall    string `json:"rollCall"`
+    FirstName   string `json:"firstName"`
+    LastName    string `json:"lastName"`
+    Major       string `json:"major"`
+    ClassName   string `json:"class"`
+    Status      string `json:"status"`
+}
+
+// Helper function to scan SQL row and create new BrotherStatusFromSemester instance 
+func CreateBrotherStatusFromSemesterFromRow(row *sql.Rows) (BrotherStatusFromSemester, error) {
+    var b BrotherStatusFromSemester
+	err := row.Scan(
+        &b.RollCall,
+        &b.FirstName,
+        &b.LastName,
+        &b.Major,
+        &b.ClassName,
+        &b.Status,
+	)
+	if err != nil {
+		return BrotherStatusFromSemester{}, err
+	}
+
+	return b, err
 }
