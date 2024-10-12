5b31880f75481de2fc27944eb0cb309a85410912
Nick Ahn <nickahn01@gmail.com>
2024-10-12 21:35:42 +0900

    Change GET  /statuses to return status labels

--- api/handlers/status_handler.go
+++ api/handlers/status_handler.go
@@ -2,7 +2,7 @@ package handlers
 
 import (
 	"context"
-	"database/sql"
+	//"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -12,74 +12,20 @@ import (
 	"github.com/pacific-theta-tau/tt-db/api/models"
 )
 
-// GET /semesters?semester=[optional]
-func (h *Handler) GetAllBrotherStatuses(w http.ResponseWriter, r *http.Request) {
-    ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
-	defer cancel()
-    semester := r.URL.Query().Get("semester")
-
-    // Query for all brother statuses
-    query := `
-    SELECT b.brotherID, b.rollCall, b.firstName, b.lastName, bs.status, s.semesterLabel
-    FROM brotherStatus bs
-    JOIN brothers b ON b.brotherID = bs.brotherID
-    JOIN semester s ON s.semesterID = bs.semesterID
-    `
-    var rows *sql.Rows
-    var err error
-    if semester == "" {
-        // Query without filter
-        rows, err = h.db.QueryContext(ctx, query)
-    } else {
-        // query filtering by semester
-        query += " WHERE semesterLabel = $1"
-        rows, err = h.db.QueryContext(
-            ctx,
-            query,
-            semester,
-        )
-    } 
-
-    // Error handling query
-    fmt.Printf("Querying for all semester statuses:\n%s\n", query)
-    if err != nil {
-        errMsg := fmt.Sprintf("Error while querying for all brother statuses: %s", err.Error())
-        log.Println(errMsg)
-        http.Error(w, errMsg, http.StatusInternalServerError)
-        return
-    }
-
-    log.Println("Parsing brother objects\n")
-    // Parse query rows
-    var brotherStatuses []*models.BrotherStatus
-    for rows.Next(){
-        brotherStatus, err := models.CreateBrotherStatusFromRow(rows)
-		if err != nil {
-            errMsg := fmt.Sprintf("Error while parsing brotherStatus query: '%s'", err.Error())
-            log.Println(errMsg)
-			http.Error(w, errMsg, http.StatusInternalServerError)
-			return
-        }
-        brotherStatuses = append(brotherStatuses, &brotherStatus)
-    }
-
-    log.Println("Building response\n")
-    // Build response
-    if err := json.NewEncoder(w).Encode(brotherStatuses); err != nil {
-        errMsg := fmt.Sprintf("Erro while encoding response: '%s'", err.Error())
-        log.Println(errMsg)
-        http.Error(w, errMsg, http.StatusInternalServerError)
-        return
-    }
-    w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+// GET /api/statuses
+func (h *Handler) GetAllStatusLabels(w http.ResponseWriter, r *http.Request) {
+    // Hardcoded since we don't plan to modify brotherStatus table in databse
+    statusLabels := [6]string{"Active", "Pre-Alumnus", "Alumnus", "Co-op", "Transferred", "Expelled"}
+    models.RespondWithSuccess(w, http.StatusOK, statusLabels)
 }
 
+
 // TODO: this is the same as POST `/api/brothers/{id}/statuses endpoint. Refactor or remove one
+//      make POST /api/brohters/statuses body receive a `brotherID`, while POST /api/brothers/{id}/statuses uses urlParams
 /* POST /statuses */
 func (h *Handler) CreateStatusForBrother(w http.ResponseWriter, r *http.Request) {
     ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
-	defer cancel()
+    defer cancel()
 
     var requestBody struct {
         BrotherID   int `json:"brotherID"`
@@ -94,6 +40,7 @@ func (h *Handler) CreateStatusForBrother(w http.ResponseWriter, r *http.Request)
         http.Error(w, errMsg, http.StatusInternalServerError)
         return
     }
+
     // Validate received data
     validate := validator.New()
     if err := validate.Struct(requestBody); err != nil {
