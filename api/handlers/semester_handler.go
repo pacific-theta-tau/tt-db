1f9a644b41895a2428109a70933494c86e47b748
Nick Ahn <nickahn01@gmail.com>
2024-10-13 00:10:51 +0900

    Change /semester endpoint to return list of labels

--- api/handlers/semester_handler.go
+++ api/handlers/semester_handler.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/go-chi/chi"
 	"github.com/pacific-theta-tau/tt-db/api/models"
 )
 
@@ -17,7 +18,7 @@ func (h *Handler) GetAllSemesterLabels(w http.ResponseWriter, r *http.Request) {
     ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
 
-    query := `SELECT semesterID, semesterLabel FROM semester`
+    query := `SELECT semesterLabel FROM semester`
     rows, err := h.db.QueryContext(ctx, query)
     log.Printf("Querying for semester labels:\n%s", query)
     if err != nil {
@@ -27,30 +28,26 @@ func (h *Handler) GetAllSemesterLabels(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    var semesterLabels []*models.Semester
+    //var semesterLabels []*models.Semester
+    var semesterLabels []*string
     for rows.Next() {
-        semester, err := models.CreateSemesterFromRow(rows)
+        var label string
+        err := rows.Scan(&label)
         if err != nil {
-            errMsg := fmt.Sprintf("Error creating Brother object from row: '%s'\n", err.Error())
+            errMsg := fmt.Sprintf("Error creating Semester label slice from row: '%s'\n", err.Error())
 			log.Println(errMsg)
 			http.Error(w, errMsg, http.StatusInternalServerError)
 			return
 		}
 
-        semesterLabels = append(semesterLabels, &semester)
+        semesterLabels = append(semesterLabels, &label)
     }
     log.Println("Parsed semester data successfully")
+    log.Println(semesterLabels)
 
     log.Println("Building response\n")
     // Build response
-    if err := json.NewEncoder(w).Encode(semesterLabels); err != nil {
-        errMsg := fmt.Sprintf("Erro while encoding response: '%s'", err.Error())
-        log.Println(errMsg)
-        http.Error(w, errMsg, http.StatusInternalServerError)
-        return
-    }
-    w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+    models.RespondWithSuccess(w, http.StatusOK, semesterLabels)
 }
 
 
@@ -85,3 +82,47 @@ func (h *Handler) CreateSemesterLabel(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Created new semester label successfully!"))
 } 
+
+func (h *Handler) GetAllBrotherStatusesForSemester(w http.ResponseWriter, r *http.Request) {
+    ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+    defer cancel()
+
+    // urlParams: if none provided, get for all semesters
+    semester := chi.URLParam(r, "semester")
+    log.Printf("Semester url param: %s", semester)
+    if semester == "" {
+        errMsg := "Missing semester in query params"
+        log.Println(errMsg)
+        models.RespondWithFail(w, http.StatusBadRequest, errMsg)
+        return
+    }
+
+    query := `
+    SELECT b.rollCall, b.firstName, b.lastName, b.major, b.className, bs.status
+    FROM brotherStatus bs
+    JOIN brothers b ON b.brotherID = bs.brotherID
+    JOIN semester s ON s.semesterID = bs.semesterID
+    WHERE semesterLabel = $1
+    `
+    rows, err := h.db.QueryContext(ctx, query, semester)
+    if err != nil {
+        errMsg := fmt.Sprintf("Error while querying brother statuses for semester %s: %s", semester, err.Error())
+        log.Println(errMsg)
+        models.RespondWithError(w, http.StatusInternalServerError, errMsg)
+        return
+    }
+
+    var brotherStatuses []*models.BrotherStatusFromSemester
+    for rows.Next() {
+        brotherStatus, err := models.CreateBrotherStatusFromSemesterFromRow(rows)
+        if err != nil {
+            errMsg := fmt.Sprintf("Error while parsing query: %s", err.Error())
+            log.Println(errMsg)
+            models.RespondWithError(w, http.StatusInternalServerError, errMsg)
+            return
+        }
+        brotherStatuses = append(brotherStatuses, &brotherStatus)
+    }
+
+    models.RespondWithSuccess(w, http.StatusOK, brotherStatuses)
+}
