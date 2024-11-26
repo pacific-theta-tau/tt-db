dd26a1a85f93f2e03d03da4aeba02d879ff16669
Nick Ahn <nickahn01@gmail.com>
2024-11-26 21:08:17 +0900

    Add helper function to make any http request method type

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:8080"
+const BASEURL = "http://localhost:8080"
 
 export interface ApiResponse<T> {
     status: string,
@@ -7,7 +7,7 @@ export interface ApiResponse<T> {
 }
 
 export const getData = async <T>(endpoint: string, urlParams?: Record<string, string>): Promise<T> => {
-    const url = new URL(endpoint, baseURL);
+    const url = new URL(endpoint, BASEURL);
 
     // Append URL parameters if provided
     if (urlParams) {
@@ -42,13 +42,51 @@ export const requestPOST = async <T>(endpoint: string, body: string): Promise<T>
             'Content-Type': 'application/json',
         }
     });
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
 
     if (!response.ok) {
         throw new Error(`Failed to fetch data: ${response.statusText}`);
     }
 
     return response.json();
+}
+
+export type ApiRequestOptions = {
+    method?: 'GET' | 'POST' | 'PUT' | 'DELETE';
+    body?: any;
+    queryParams?: any;
+}
+
+export const request = async <T>(
+    endpoint: string,
+    method?: 'GET' | 'POST' | 'PUT' | 'DELETE',
+    body?: Record<string, unknown>,
+    queryParams?: any,
+): Promise<T> => {
+    const url = new URL(endpoint, BASEURL)
+
+    if (queryParams) {
+        Object.keys(queryParams).forEach(key => {
+            url.searchParams.append(key, queryParams[key])
+        })
+    }
+
+    const response = await fetch(
+        url.toString(),
+        {
+            method: method? method : 'GET', // Default method = GET
+            body: JSON.stringify(body),
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+    )
+
+    if (!response.ok) {
+        const errorMessage = `API request error: ${response.statusText}`
+        console.error(errorMessage)
+        throw new Error(errorMessage);
+    }
+
+    return response.json()
 }
