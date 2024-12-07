db79caec0c4f0b6cb8ad53c1b277ad52beecba57
Nick Ahn <nickahn01@gmail.com>
2024-12-07 18:32:40 +0900

    Add ReactQuery to members page

--- frontend/src/components/brothers-table.tsx
+++ frontend/src/components/brothers-table.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useQuery } from "@tanstack/react-query";
 import { Brother, brothersTableColumns } from "./columns"
 import { DataTable } from "./data-table"
 import { Skeleton } from "@/components/ui/skeleton"
@@ -7,33 +8,26 @@ import SideRowSheet from './sheet/side-row-sheet';
 import { ApiResponse, getData } from '../api/api'
 
 
-const BrothersTable: React.FC = () => {
-    const [data, setData] = useState<Brother[]>([]);
-    const [loading, setLoading] = useState<boolean | null>(true);
-    const [error, setError] = useState<string | null>(null);
-
-    useEffect(() => {
+async function fetchTableData() {
         const endpoint = "http://localhost:8080/api/brothers"
-        const fetchData = async () => {
-             try {
-                setLoading(true)
-                const result: ApiResponse<Brother[]> = await getData(endpoint)
-                console.log('result:', result)
-                setData(result.data);
-            } catch (error: any) {
-                setError((error as Error).message);
-                console.log('Error fetching data:', error);
-                throw error;
-            } finally {
-                /* uncomment line below to test skeleton during loading */
-                // await new Promise(f => setTimeout(f, 3000));
-                setLoading(false);
-            }
+        try {
+            const result: ApiResponse<Brother[]> = await getData(endpoint)
+            console.log('result:', result)
+            return result.data
+        } catch (error: any) {
+            console.log('Error fetching data:', error);
+            throw error;
         }
-        fetchData()
-       }, []);
+}
 
-    if (loading) {
+
+const BrothersTable: React.FC = () => {
+    // const [data, setData] = useState<Brother[]>([]);
+    // const [loading, setLoading] = useState<boolean | null>(true);
+    // const [error, setError] = useState<string | null>(null);
+    const { data, isLoading, isError } = useQuery({queryKey: ["brothersTableData"], queryFn: fetchTableData})
+
+    if (isLoading) {
         // Load dummy empty data and skeleton
         const loadingData = Array(5).fill({}) 
         const loadingTableColumns = brothersTableColumns.map((column) => ({
@@ -43,14 +37,14 @@ const BrothersTable: React.FC = () => {
         return <DataTable columns={ loadingTableColumns } data={loadingData} />
     }
 
-    if (error) {
-        return <div>Error: {error}</div>;
+    if (isError) {
+        return <div>Error loading table data</div>;
     }
 
     return (
         <DataTable
             columns={brothersTableColumns}
-            data={data}
+            data={data ?? []}
             AddSheet={
                 () => <SideRowSheet
                         title="Add new member record"
